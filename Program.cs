7a8f5e38b6eec43f0983d58072d66c49bc39f35c
cemanasim <asim.ceman@kodecta.com>
2021-12-27 10:00:31 +0100

    changed input

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ namespace SchematicsProject
             var engine = new Engine();
             try
             {
-                await engine.InputProcces(input2);
+                await engine.InputProcces(args);
             }
             catch (ArgumentException e)
             {
